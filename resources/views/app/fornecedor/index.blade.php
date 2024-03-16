c5331904cb44f0f9eeab5d75191f336a916669c6
Clayton <_clayton@outlook.com>
2024-03-16 10:15:14 -0300

    @isset($variavel)
    //retorna true se a variavel estiver definada

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,6 +3,17 @@
 
 {{--@dd($fornecedores)--}}
 
+{{--
+    if(isset($variavel)){} //retorna true se a variavel estiver definada
+--}}
+@isset($fornecedores)
+    Fornecedores: {{$fornecedores[0]['nome']}}
+    <br>
+    Status: {{$fornecedores[0]['status']}}
+    <br>    
+@endisset
+
+<br>
 @if(count($fornecedores)>0 && count($fornecedores)<10)
     <h3>Existem alguns Fornecedores</h3>
 @elseif(count($fornecedores)>10)
