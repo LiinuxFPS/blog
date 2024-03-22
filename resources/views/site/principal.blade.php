684ed3c4913a503cd8565c8a73da592918484920
Clayton <_clayton@outlook.com>
2024-03-21 21:28:55 -0300

    @include
    @include('dir.dir.aquivo')
    
    inclusão do menu

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,23 +1,10 @@
 @extends('site.layouts.basico')
 
 {{-- empurra todo conteudo @section para o @yield --}}
+@section('titulo', 'Principal')
 @section('conteudo')
-<div class="topo">
 
-    <div class="logo">
-        {{-- arquivos imagens dentro da pasta public --}}
-        <img src="{{ asset('/img/logo.png')}}">
-    </div>
-
-    <div class="menu">
-        <ul>
-            <li><a href="{{ route('site.index') }}">Principal</a></li>
-            <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-            <li><a href="{{ route('site.contato') }}">Contato</a></li>
-        </ul>
-    </div>
-</div>
-
+@include('site.layouts._partials.topo')
 <div class="conteudo-destaque">
 
     <div class="esquerda">
