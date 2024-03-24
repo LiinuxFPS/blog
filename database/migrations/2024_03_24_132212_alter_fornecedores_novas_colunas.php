d4316ebb8b0b0f2c3e1e43c3c47c17c6a63d45ba
Clayton <_clayton@outlook.com>
2024-03-24 10:38:02 -0300

    model && migration Forncedor
    AlterFornecedoresNovasColunas->model criada para editar/incluir campos na tabela fornecedor
    
    //Schema::create -> cria uma tabela
    //Schema::table-> cria/edita novos campos na tabela ja existentes

---
+++ database/migrations/2024_03_24_132212_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Schema::create -> cria uma tabela
+        //Schema::table-> cria/edita novos campos na tabela ja existentes
+        Schema::table('fornecedors', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
